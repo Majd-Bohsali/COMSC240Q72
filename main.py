0c1c0dc165f6f7ea757cc3e2d933e5d0e25ec71f
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2024-10-17 20:11:41 -0700

    completed code

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ def getSum(numbers, n):
      
     sumsList = []
     for startIndex in range(0, len(numbers) - n + 1): 
-        sums = reduce(lambda x, y: x + y, numbers[startIndex: startIndex + n])
-        print(sum)
+        sumsList.append(reduce(lambda x, y: x + y, numbers[startIndex: startIndex + n]))
     
-    return 0
+    return max(sumsList)
 
 
 if __name__ == "__main__":
-    numbers = [1, 2, 3, 4 ,5]
+    numbers = [1, 2, 3, 4, 5]
     n = 3
-    getSum(numbers, n)
+    print("Numbers: ", numbers, "n: ", n, "Maximum sum: ", getSum(numbers, n))
+    
+    numbers = [9, 10, 11, 10, 2]
+    n = 3
+    print("Numbers: ", numbers, "n: ", n, "Maximum sum: ", getSum(numbers, n))
+    
+    numbers = [5, 4, 3, 2, 1]
+    n = 3
+    print("Numbers: ", numbers, "n: ", n, "Maximum sum: ", getSum(numbers, n))
+    
+    numbers = [9, 10, 11, 10, 2]
+    n = 1
+    print("Numbers: ", numbers, "n: ", n, "Maximum sum: ", getSum(numbers, n))
